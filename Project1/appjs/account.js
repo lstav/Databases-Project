7ada98fc82e91cb0040b7112b6c4a7f57a745692
luistavarez <luis.tavarez@upr.edu>
2013-09-24 23:37:13 -0400

    Added account and app js, as well as account.html

---
+++ Project1/appjs/account.js
@@ -0,0 +1,8 @@
+function Account(customerName, accountNumber, mailingAddress, billingAddress, creditCard, rank){
+	this.customerName = customerName;
+	this.accountNumber = accountNumber;
+	this.mailingAddress = mailingAddress;
+	this.billingAddress = billingAddress;
+	this.creditCard = creditCard;
+	this.rank = rank;
+}
---
+++ Project1/appjs/app.js
@@ -0,0 +1,19 @@
+var accountList = new Array(
+	new Account("Luis", "123", "Puerto Rico", "Puerto Rico", "321", "****")
+);
+
+$(document).on('pagebeforeshow', "#accounts", function( event, ui ) {
+	var len = accountList.length;
+	var list = $("#account-list");
+	list.empty();
+	var account;
+	for (var i=0; i < len; ++i){
+		account = accountList[i];
+		list.append("<h2>" + account.customerName + "</h2>" + 
+			"<p><strong>Number: " + account.accountNumber +  "</strong></p>" + 
+			"<p> Mailing Address: " + account.mailingAddress + "</p>" + 
+			"<p> Billing Address: " + account.billingAddress + "</p>" +
+			"<p class=\"ui-li-aside\"> Rank: " + account.rank + "</p>");
+	}
+	list.listview("refresh");
+});
