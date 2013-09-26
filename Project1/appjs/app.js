abf368158e878863d60c256cddd7a08260c46917
luistavarez <luis.tavarez@upr.edu>
2013-09-25 23:54:30 -0400

    Changed menu to the same type as in categories, made some changes in app to test interaction between js and jquery

--- Project1/appjs/app.js
+++ Project1/appjs/app.js
@@ -1,5 +1,5 @@
 var accountList = new Array(
-	new Account("Luis", "123", "Puerto Rico", "Puerto Rico", "321", "****")
+	new Account("Luis", "123", "Puerto Rico", "Puerto Rico", "987654321", "****")
 );
 
 $(document).on('pagebeforeshow', "#accounts", function( event, ui ) {
@@ -13,6 +13,7 @@ $(document).on('pagebeforeshow', "#accounts", function( event, ui ) {
 			"<p><strong>Number: " + account.accountNumber +  "</strong></p>" + 
 			"<p> Mailing Address: " + account.mailingAddress + "</p>" + 
 			"<p> Billing Address: " + account.billingAddress + "</p>" +
+			"<p> Credit Card: *****" + account.creditCard.substr(5,6) + "</p>" +
 			"<p class=\"ui-li-aside\"> Rank: " + account.rank + "</p>");
 	}
 	list.listview("refresh");
