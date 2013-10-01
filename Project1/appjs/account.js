b4b1cb9cbadf8ddc29c78f7897c4407a99b530bc
luistavarez <luis.tavarez@upr.edu>
2013-09-30 23:55:24 -0400

    function for server

--- Project1/appjs/account.js
+++ Project1/appjs/account.js
@@ -1,8 +1,10 @@
 function Account(customerName, accountNumber, mailingAddress, billingAddress, creditCard, rank){
+	this.id = "";
 	this.customerName = customerName;
 	this.accountNumber = accountNumber;
 	this.mailingAddress = mailingAddress;
 	this.billingAddress = billingAddress;
 	this.creditCard = creditCard;
 	this.rank = rank;
+	this.toJSON = toJSON;
 }
