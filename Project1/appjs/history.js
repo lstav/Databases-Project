fdfb46be671b151462b911f4f0f2748a7c123ba1
Lexter Seda <Lexter.seda@upr.edu>
2013-10-06 15:29:10 -0400

    history

---
+++ Project1/appjs/history.js
@@ -0,0 +1,6 @@
+function History(aid, productList){
+	this.hid = "";
+	this.aid = aid;
+	this.productList = productList;
+	this.toJSON = toJSON;
+}
---
+++ Project1Srv/history.js
@@ -0,0 +1,7 @@
+module.exports =  { 
+	History : function (aid, productList){
+		this.hid = "";
+		this.aid = aid;
+		this.productList = productList;
+	}
+}
