456e60bd18d3ade40e6288ef62f3761292e1abf5
Lexter Seda <Lexter.seda@upr.edu>
2013-10-07 15:30:49 -0400

    js de ShoppingCart

---
+++ Project1/appjs/shoppingcart.js
@@ -0,0 +1,6 @@
+function ShoppingCart(aid, productList){
+	this.scid = "";
+	this.aid = aid;
+	this.productList = productList;
+	this.toJSON = toJSON;
+}
---
+++ Project1Srv/shoppingcart.js
@@ -0,0 +1,7 @@
+module.exports =  { 
+	ShoppingCart : function (aid, productList){
+		this.scid = "";
+		this.aid = aid;
+		this.productList = productList;
+	}
+}
